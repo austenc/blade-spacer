d01f6eb8c8eb25d98907e74216b8ed85835965f7
austenc <austen.cameron@gmail.com>
2014-06-13 02:00:57 -0600

    added handling for triple-curly syntax

--- BladeSpacer.py
+++ BladeSpacer.py
@@ -5,16 +5,35 @@ class BladeSpacerCommand(sublime_plugin.TextCommand):
 		for sel in self.view.sel():
 
 			# the previously typed letter position
-			first = sel.end()
+			last = sel.end()
 
 			# insert any closing brackets like sublime normally does
 			self.view.run_command('insert_snippet', {"contents": "{$0}"})
 
-			if(self.view.substr(first-1) == '{'):
-				# add 2 spaces
-				self.view.insert(edit, first+1, '  ')
+			print(self.view.substr(last-1)+'| 1')
+			print(self.view.substr(last-2)+'| 2')
 
-				# move cursor to middle
-				middle = first+2
-				self.view.sel().clear()
-				self.view.sel().add(sublime.Region(middle, middle))
+			# was the last typed character a curly brace?
+			if(self.view.substr(last-1) == '{'):
+				self.addSpaces(edit, last)
+
+			# probably a triple curly brace
+			elif(self.view.substr(last-1) == ' ' and self.view.substr(last-2) == '{'):
+				# erase previous space
+				self.view.erase(edit, sublime.Region(last, last-1))
+				
+				# erase latter space
+				self.view.erase(edit, sublime.Region(last+1, last+2))
+
+				# add two spaces and center! write function 
+				self.addSpaces(edit, last-1)
+
+
+	def addSpaces(self, edit, pos):
+		# add 2 spaces
+		self.view.insert(edit, pos+1, '  ')
+
+		# move cursor to middle
+		middle = pos+2
+		self.view.sel().clear()
+		self.view.sel().add(sublime.Region(middle, middle))
