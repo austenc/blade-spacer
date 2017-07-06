eb82b449b4aaeac7c1fd8d6ffb1b0545da3f83d2
austenc <austen.cameron@gmail.com>
2017-07-06 07:48:00 -0600

    added missing closing brace example / test

--- test.blade.php
+++ test.blade.php
@@ -17,3 +17,6 @@
 {{{test }}}
 {!!test !!}
 {{--test --}}
+
+{{-- Test for missing end braces - '@' represents cursor position --}}
+<h2>Skill Test #{{ $skill->id }} <small>for <a href="@"></a></small></h2>
